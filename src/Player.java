6512b8846d85a96920c215f1d97635d83f9eec97
cs.park <pcs0331@naver.com>
2019-05-09 13:40:04 +0900

    no message

--- src/Player.java
+++ src/Player.java
@@ -15,6 +15,18 @@ public class Player {
 		}
 	}
 	
+	public void printPlayerCardList(){
+		StringBuilder sb = new StringBuilder();
+		String name = getName();
+		sb.append("현재" +name+"카드 목록\n");
+		
+		for(Card card : cards){
+			sb.append(card.toString());
+			sb.append("\n");
+		}
+		System.out.println(sb.toString());
+	}
+	
 	public boolean holdingJokerYN(){
 		for(Card card : cards){
 			if(card.getNumber().equals("0")){
